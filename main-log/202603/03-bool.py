b2198d2cded75579d319503f0a7913cb4f817082
Itoyoroshika <Jyuri.sama.sama@gmail.com>
2026-03-03 19:13:11 +0900

    Day 2 bool

--- main-log/202603/03-bool.py
+++ main-log/202603/03-bool.py
@@ -1,2 +1,19 @@
-score = int(input("試験の天数を入力"))
+# ーーーbool型ーーー
+score = int(input("試験の点数を入力"))
 print(score >= 60)
+
+# ーーー論理演算子ーーー
+'''
+and or not などを論理演算子と呼ぶ
+'''
+if not (score < 60, score >100):
+    print("不合格")
+else:
+    print("合格")
+
+# 以下のような条件式も可能だが基本はtrue/falseで評価
+score = 0
+if score:  # score != 0 と同義
+    print("処理")
+else:
+    print("処理")
